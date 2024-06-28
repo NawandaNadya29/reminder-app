970a44ff7180393fde0403678860c8287b8402d1
Nadya <74343587+NawandaNadya29@users.noreply.github.com>
2024-06-28 22:17:45 +0700

    Update page.jsx

--- app/editTopic/page.jsx
+++ app/editTopic/page.jsx
@@ -3,7 +3,7 @@ import EditTopicForm from "@/components/EditTopicForm";
 const getTopicById = async (id) => {
   try {
     const res = await fetch(`http://localhost:3000/api/topics/?id=${id}`, {
-      cache: "no-todo",
+      cache: "no-cacahe",
     });
 
     if (!res.ok) {
